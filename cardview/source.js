1c2c8be637a11a8af67cd70db915cdcef5bd5e95
kim <bear2u@naver.com>
2023-03-27 21:57:03 +0900

    fix

--- cardview/source.js
+++ cardview/source.js
@@ -4,6 +4,7 @@ var colorArray=["#019875","#1E8BC3","#D91E18","#D35400","#8E44AD","#C0392B"];
 var cardState;
 var currentQuestion=0;
 var qbank=new Array;
+var answerBank = new Array;
 
 loadDB();
 
@@ -15,7 +16,8 @@ function loadDB(){
     let num2 = Math.floor(Math.random()*9)+1;
     qbank[i]=[];
     qbank[i][0]=num1 + "+" + num2;
-    qbank[i][1]=num1 + num2;
+    qbank[i][1]=num2 + "+" + num1;
+    answerBank[i] = num1+num2;
  }//for
     beginActivity(); 
 }//loadDB
@@ -26,7 +28,7 @@ function beginActivity(){
  var color1=colorArray[Math.floor(Math.random()*colorArray.length)];
  $("#cardArea").empty();
  $("#cardArea").append('<div id="card1" class="card">' + qbank[currentQuestion][0] + '</div>');
- $("#cardArea").append('<div id="card2" class="card">' + qbank[currentQuestion][1] + '</div>');
+ $("#cardArea").append('<div id="card2" class="card">' + qbank[currentQuestion][1] + '</div>'); 
  $("#card1").css("background-color",color1);
  $("#card2").css("background-color","#34495E");
  $("#card2").css("top","200px");
@@ -42,8 +44,18 @@ function beginActivity(){
  $("#buttonArea").empty();
  $("#buttonArea").append('<div id="nextButton">NEXT</div>');
  $("#nextButton").on("click",function(){
-  if(currentQuestion<qbank.length){beginActivity();}
-  else{displayFinalMessage();}
+     //정답 체크
+  let ab = answerBank[currentQuestion-1];
+  let answer = $('#answer').val();
+  if(ab == parseInt(answer)) {
+      $('#answer').val('');
+      $('#answer').css('backgroundColor','#019875');
+      if(currentQuestion<qbank.length){beginActivity();}
+      else{displayFinalMessage();}
+  } else{     
+     $('#answer').css('backgroundColor','red')
+  }
+//   alert(currentQuestion-1 + "," + ab +"=" + answer);
  });//click function
 }//beginactivity
 
